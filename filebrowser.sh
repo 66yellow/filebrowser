6750fce743c547d358d04b052654012108d8cbc4
66yellow <563643241@qq.com>
2021-01-08 10:50:42 +0800

    Update filebrowser.sh

--- filebrowser.sh
+++ filebrowser.sh
@@ -67,7 +67,7 @@ WantedBy=multi-user.target
 		mkdir -p /etc/filebrowser
 		cat >/etc/filebrowser/filebrowser.json <<-EOF
 {
-    "port": 66,
+    "port": 80,
     "baseURL": "",
     "address": "",
     "log": "stdout",
@@ -84,7 +84,7 @@ WantedBy=multi-user.target
 		echo -e "
 		Filebrowser 安装完成啦！
 
-		预览地址: ${yellow}http://${ip}:66/$none
+		预览地址: ${yellow}http://${ip}/$none
 
 		用户名: ${green}admin$none
 
